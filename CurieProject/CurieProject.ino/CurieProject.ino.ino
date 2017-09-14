842d79c50778f030b3166bc4df8c4c5e43c9f581
giovanni_ciandrini <giovanni.ciandrini@scai-consulting.grupposcai.it>
2017-09-14 17:15:44 +0200

    Sketch updated with auth_characteristic

--- CurieProject/CurieProject.ino/CurieProject.ino.ino
+++ CurieProject/CurieProject.ino/CurieProject.ino.ino
@@ -1,5 +1,7 @@
 #include "CurieIMU.h"
 #include "CurieBLE.h"
+#include "HexConversionUtils.h"
+#include "SipHash_2_4.h"
 #include <stdio.h>      /* puts, printf */
 
 int aix, aiy, aiz;    // raw accelerometer values
@@ -16,11 +18,15 @@ BLEFloatCharacteristic gxCharacteristic("19B10015-E8F2-537E-4F6C-D104768A1214", 
 BLEFloatCharacteristic gyCharacteristic("19B10016-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify); // rot.in y in deg/s
 BLEFloatCharacteristic gzCharacteristic("19B10017-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify); // rot.in z in deg/s
 BLEIntCharacteristic shockCharacteristic("19B10018-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);  // true if shock sensed
-BLEIntCharacteristic modeCharacteristic("19B10019-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);  // sensing mode
+BLELongCharacteristic authCharacteristic("9B100124-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);  // auth_characteristic
+/*BLEIntCharacteristic modeCharacteristic("19B10019-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);  // sensing mode
 BLEIntCharacteristic srangeCharacteristic("19B10020-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);  // shock range
 BLEIntCharacteristic arangeCharacteristic("19B10021-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);  // acc. range
 BLEIntCharacteristic grangeCharacteristic("9B100122-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite); // gyro range
 BLEIntCharacteristic calibCharacteristic("9B100123-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);  // 1 if calibration is on, otherwise 0
+*/
+
+const uint8_t key[] PROGMEM = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
 
 
 
@@ -47,6 +53,7 @@ void setup()
   // blePeripheral.addAttribute(srangeCharacteristic);
   // blePeripheral.addAttribute(grangeCharacteristic);*/
   // blePeripheral.addAttribute(calibCharacteristic);
+    blePeripheral.addAttribute(authCharacteristic);
   // set initial characteristics values
   stepCharacteristic.setValue(0);
   shockCharacteristic.setValue(0);
@@ -55,6 +62,7 @@ void setup()
   srangeCharacteristic.setValue(4);
   grangeCharacteristic.setValue(250);
   calibCharacteristic.setValue(1);
+  authCharacteristic.setValue(0);
   blePeripheral.begin();
   /*if (calibCharacteristic.value() == 1) {
     //Serial.println("About to calibrate. Make sure your board is stable and upright");
@@ -82,13 +90,18 @@ void setup()
   //CurieIMU.interrupts(CURIE_IMU_MOTION);
   CurieIMU.attachInterrupt(eventCallback);
 
+  //HANDLING KEY
+  
+ // sipHash.initFromPROGMEM(key);
+  
+  //  printKey((uint8_t*)key);
 }
 
 void loop()
 {
   if(!flag){
-    CurieIMU.setAccelerometerRange(arangeCharacteristic.value());
-    Serial.println(CurieIMU.getAccelerometerRange());
+   /* CurieIMU.setAccelerometerRange(arangeCharacteristic.value()); //  Stampare quando compro adattatore per BLE e clono indirizzo MAC 
+    Serial.println(CurieIMU.getAccelerometerRange()); */
     CurieIMU.readMotionSensor(aix, aiy, aiz, gix, giy, giz);
     flag = true;  
   }else{
@@ -128,12 +141,38 @@ void loop()
   shockCharacteristic.setValue(0);*/
 }
 
+/*void printKey(unsigned char PROGMEM *key) {
+  Serial.print(F(" Key:"));
+  unsigned char tmpKey[16];
+  memcpy_P(tmpKey,key,16);
+  char tmp[33];
+  hexToAscii((const unsigned char*)tmpKey,16,tmp,33);
+  Serial.println(tmp);
+}*/
 void eventCallback() {
   if (CurieIMU.getInterruptStatus(CURIE_IMU_SHOCK)) {
     shockCharacteristic.setValue(1);
   }
 
 }
+
+
+/*
+float AnalyzeMessage(Message m){}
+
+float checkForAuthorization(hash1,hash2){}
+
+unsigned char* calculateHashWithKey(Message m){}
+
+void storeMAC(MacAdress mac){}
+
+void showRealCharacteristic(){}
+
+void showFakeCharacteristic(){}
+
+*/
+
+
 
 // converted values based on accelerometer range
 float convertRawAcceleration(int aRaw) {
